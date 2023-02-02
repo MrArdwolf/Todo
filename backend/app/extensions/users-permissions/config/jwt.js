ea80cc49c5161dc56c1a98f3562ff9b7c67099c7
Elias Öman <elias@folkestorp.se>
2023-02-02 15:22:35 +0100

    backend working

---
+++ backend/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a9078f6-03fb-4e85-8948-836a339c07a8'
+};
---
+++ backend2/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3288b35c-4625-49e2-9a35-71757c4f5271'
+};
