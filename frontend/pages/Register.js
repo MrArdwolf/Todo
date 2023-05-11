2112dd95c116374575936d4f4342d94f2ef76027
Elias Öman <elias@folkestorp.se>
2023-05-11 14:10:08 +0200

    e

--- frontend/pages/Register.js
+++ frontend/pages/Register.js
@@ -43,64 +43,64 @@ export default function Login() {
     return (
         <>
             <main>
-                <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-                    <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-3xl">
-                        <div className="mb-4">
-                            <h1 className="p-2 text-gray-950 cursor-pointer	" onClick={() => Router.push('/')}>Att Göra Lista</h1>
-                            <div className="grid my-4 mx-7 justify-items-center">
-                                <div className='flex justify-between'>
-                                    <h1 className="text-gray-950 mr-14 font-medium">Registrera</h1>
-                                    <a className="text-gray-950 ml-4 cursor-pointer font-light" onClick={() => Router.push('/')}>Har du ett konto?</a>
-                                </div>
-                                <input
-                                    type="tex"
-                                    className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 mt-5 text-gray-900"
-                                    placeholder="Användarnamn"
-                                    onChange={e => setRegUsername(e.target.value)}
-                                    value={regUsername}
-                                />
-                                <input
-                                    type="email"
-                                    className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 my-5 text-gray-900"
-                                    placeholder="Email"
-                                    onChange={e => setRegEmail(e.target.value)}
-                                    value={regEmail}
+                    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+                        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-3xl">
+                            <div className="mb-4">
+                                <h1 className="p-2 text-gray-950 cursor-pointer	" onClick={() => Router.push('/')}>Att Göra Lista</h1>
+                                <div className="grid my-4 mx-7 justify-items-center">
+                                    <div className='flex justify-between'>
+                                        <h1 className="text-gray-950 mr-6 font-medium">Registrera</h1>
+                                        <a className="text-gray-950 ml-6 cursor-pointer font-light" onClick={() => Router.push('/')}>Har du ett konto?</a>
+                                    </div>
+                                    <input
+                                        type="tex"
+                                        className="shadow appearance-none border-2 rounded w-64 py-2 px-3 mt-5 text-gray-900"
+                                        placeholder="Användarnamn"
+                                        onChange={e => setRegUsername(e.target.value)}
+                                        value={regUsername}
+                                    />
+                                    <input
+                                        type="email"
+                                        className="shadow appearance-none border-2 rounded w-64 py-2 px-3 mt-5 text-gray-900"
+                                        placeholder="Email"
+                                        onChange={e => setRegEmail(e.target.value)}
+                                        value={regEmail}
 
-                                />
-                                <input
-                                    type="password"
-                                    className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 mb-5 text-gray-900"
-                                    placeholder="Lösenord"
-                                    onChange={e => setRegPassword(e.target.value)}
-                                    value={regPassword}
-                                    onKeyDown={(e) => {
-                                        if (e.key === "Enter") {
-                                            handleRegister();
-                                        }
-                                    }}
-                                />
-                                <div className='flex justify-center'>
-                                    <button id='login_button' onClick={() => handleRegister()} className="flex-none p-2 mr-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Registrera</button>
+                                    />
+                                    <input
+                                        type="password"
+                                        className="shadow appearance-none border-2 rounded w-64 py-2 px-3 my-5 text-gray-900"
+                                        placeholder="Lösenord"
+                                        onChange={e => setRegPassword(e.target.value)}
+                                        value={regPassword}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                handleRegister();
+                                            }
+                                        }}
+                                    />
+                                    <div className='flex justify-center'>
+                                        <button id='login_button' onClick={() => handleRegister()} className="flex-none w-64 px-3 py-2 border-2 rounded-xl text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Registrera</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='grid justify-items-center'>
-                                <a>---------------------- Eller ----------------------</a>
-                                <a href={`${backendUrl}/api/connect/discord`}>
-                                    <button
-                                        onClick={() => handleproviderlogin('discord')}
-                                        className="items-center px-5 flex-none py-2 ml-x mb-2 mt-4 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Discord
-                                    </button>
-                                </a>
-                                <a href={`${backendUrl}/api/connect/google`}>
-                                    <button
-                                        onClick={() => handleproviderlogin('google')}
-                                        className="items-center flex flex-row justify-center px-5 flex-none py-2 ml-x mt-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Google
-                                    </button>
-                                </a>
+                                <div className='grid justify-items-center'>
+                                    <a>---------------------- Eller ----------------------</a>
+                                        <a href={`${backendUrl}/api/connect/discord`}>
+                                            <button
+                                                onClick={() => handleproviderlogin('discord')}
+                                                className="items-center px-3 flex-none py-2 ml-x mb-2 mt-4 border-2 rounded-xl w-64 text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Discord
+                                            </button>
+                                        </a>
+                                        <a href={`${backendUrl}/api/connect/google`}>
+                                            <button
+                                                onClick={() => handleproviderlogin('google')}
+                                                className="items-center flex flex-row justify-center px-3 flex-none py-2 ml-x mt-2 border-2 rounded-xl w-64 text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Google
+                                            </button>
+                                        </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
             </main>
         </>
     )
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -92,18 +92,19 @@ export default function Login() {
         return (
             <>
                 <main>
+                    
                     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
                         <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-3xl">
                             <div className="mb-4">
                                 <h1 className="p-2 text-gray-950 cursor-pointer	" onClick={() => Router.push('/')}>Att Göra Lista</h1>
                                 <div className="grid my-4 mx-7 justify-items-center">
                                     <div className='flex justify-between'>
-                                        <h1 className="text-gray-950 mr-14 font-medium">Logga In</h1>
-                                        <a className="text-gray-950 ml-14 cursor-pointer font-light" onClick={() => Router.push('/Register')}>Inget Konto?</a>
+                                        <h1 className="text-gray-950 mr-12 font-medium">Logga In</h1>
+                                        <a className="text-gray-950 ml-12 cursor-pointer font-light" onClick={() => Router.push('/Register')}>Inget Konto?</a>
                                     </div>
                                     <input
                                         type="tex"
-                                        className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3  mt-5 text-gray-900 "
+                                        className="shadow appearance-none border-2 w-64 py-2 px-3  mt-5 text-gray-900 "
                                         placeholder="Användarnamn/Email"
                                         onChange={e => setUsername(e.target.value)}
                                         value={username}
@@ -116,7 +117,7 @@ export default function Login() {
                                     />
                                     <input
                                         type="password"
-                                        className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3  my-5 text-gray-900"
+                                        className="shadow appearance-none border-2 rounded w-64 py-2 px-3 my-5 text-gray-900"
                                         placeholder="Lösenord"
                                         onChange={e => setPassword(e.target.value)}
                                         value={password}
@@ -128,7 +129,7 @@ export default function Login() {
 
                                     />
                                     <div className='flex justify-center'>
-                                        <button id='login_button' onClick={() => handleLogin()} className="flex-none p-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Logga in</button>
+                                        <button id='login_button' onClick={() => handleLogin()} className="flex-none w-64 px-3 py-2 border-2 rounded-xl text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Logga in</button>
                                     </div>
                                 </div>
 
@@ -138,13 +139,13 @@ export default function Login() {
                                     <a href={`${backendUrl}/api/connect/discord`}>
                                         <button
                                             onClick={() => handleproviderlogin('discord')}
-                                            className="items-center px-5 flex-none py-2 ml-x mb-2 mt-4 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Discord
+                                            className="items-center px-3 flex-none py-2 ml-x mb-2 mt-4 border-2 rounded-xl w-64 text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Discord
                                         </button>
                                     </a>
                                     <a href={`${backendUrl}/api/connect/google`}>
                                         <button
                                             onClick={() => handleproviderlogin('google')}
-                                            className="items-center flex flex-row justify-center px-5 flex-none py-2 ml-x mt-2 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Google
+                                            className="items-center flex flex-row justify-center px-3 flex-none py-2 ml-x mt-2 border-2 rounded-xl w-64 text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Fortsätt med Google
                                         </button>
                                     </a>
                                     <a id='ForgottenPassword' onClick={() => Router.push('/ForgottenPassword')} className="flex-none mt-3 p-2 rounded text-gray-950 cursor-pointer font-light">Glömt Lösenord?</a>
@@ -157,70 +158,7 @@ export default function Login() {
         )
     }
 
-    if (register == true) {
-        return (
-            <>
-                <main>
-                    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-                        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-3xl">
-                            <div className="mb-4">
-                                <h1 className="p-2 text-gray-950 cursor-pointer	" onClick={() => Router.push('/')}>Att Göra Lista</h1>
-                                <div className=" float-right mt-4 mr-7">
-                                    <h1 className="text-gray-950">Registrera</h1>
-                                    <input
-                                        type="tex"
-                                        className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 mr-2 mt-5 text-gray-900"
-                                        placeholder="Användarnamn"
-                                        onChange={e => setRegUsername(e.target.value)}
-                                        value={regUsername}
-                                    />
-                                    <br></br>
-                                    <input
-                                        type="email"
-                                        className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 mr-2 my-5 text-gray-900"
-                                        placeholder="Email"
-                                        onChange={e => setRegEmail(e.target.value)}
-                                        value={regEmail}
-
-                                    />
-                                    <br></br>
-                                    <input
-                                        type="password"
-                                        className="shadow appearance-none border-2 rounded max-w-xl py-2 px-3 mr-2 mb-5 text-gray-900"
-                                        placeholder="Lösenord"
-                                        onChange={e => setRegPassword(e.target.value)}
-                                        value={regPassword}
-                                        onKeyDown={(e) => {
-                                            if (e.key === "Enter") {
-                                                handleRegister();
-                                            }
-                                        }}
-                                    />
-                                    <br></br>
-                                    <button id='login_button' onClick={() => handleRegister()} className="flex-none p-2 mr-7 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Registrera</button>
-
-                                </div>
-                                <div>
-                                    <a href={`${backendUrl}/api/connect/discord`}>
-                                        <button
-                                            onClick={() => handleproviderlogin('discord')}
-                                            className="items-center pl-2 pr-5 flex-none p-2 ml-7 mt-7 mr-7 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Logga in med Discord
-                                        </button>
-                                    </a>
-                                    <a href={`${backendUrl}/api/connect/google`}>
-                                        <button
-                                            onClick={() => handleproviderlogin('google')}
-                                            className="items-center flex flex-row justify-center pl-2 pr-5 flex-none p-2 ml-7 mt-7 mr-7 border-2 rounded text-teal-600 border-teal-600 hover:text-white hover:bg-teal-600">Logga in med Google
-                                        </button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </>
-        )
-    }
+    
 
 
 
